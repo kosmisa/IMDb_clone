f8fedfbbbdece51bfa1d4ec46c6b99ca2b196811
kosmisa <vukasin.jevtic@gmail.com>
2023-10-31 00:02:13 +0100

    added functionality first part

---
+++ imdb/src/app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    // <div className='fixed top-0 bottom-0 right-0 left-0  m-auto z-0 place-content-center'>
+    <div className='container mx-52 my-56 flex justify-center'>
+        <img className='h-60' src="loading_animation.svg" alt="loading..."></img>
+    </div>
+  )
+}
